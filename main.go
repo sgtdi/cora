659eb69adb2b7555b6121e14f67c94ca0e9f9e97
Alessio <asoseil@gmail.com>
2022-03-21 16:08:16 +0100

    request headers

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package cora
 
 import (
+	"bytes"
+	"errors"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
 
 type cora struct {
-	req    *http.Request
-	client *http.Client
+	req     *http.Request
+	client  *http.Client
+	host    string
+	headers []Header
+}
+
+type Header struct {
+	Name   string
+	Values []string
 }
 
 type Cora interface {
-	Get(string) Response
-	Head(string) Response
-	Post(string, []byte) Response
-	Put(string, []byte) Response
-	Delete(string) Response
-	Options(string) Response
-	Trace(string)
-	Patch(string, []byte) Response
+	Get(string, ...Header) Response
+	Head(string, ...Header) Response
+	Post(string, []byte, ...Header) Response
+	Put(string, []byte, ...Header) Response
+	Delete(string, ...Header) Response
+	Options(string, ...Header) Response
+	Trace(string, ...Header)
+	Patch(string, []byte, ...Header) Response
+	SetHost(string) Cora
+	SetHeaders(...Header) Cora
 }
 
 // New cora instance
@@ -44,100 +57,116 @@ func (c *cora) Client(client *http.Client) {
 
 // SetHost custom for a single request, apply it to all requests using Request method
 func (c *cora) SetHost(host string) Cora {
-	c.req.Host = host
+	c.host = host
 	return c
 }
 
 // SetHeaders replace headers for a single request, apply it to all requests using Request method
-func (c *cora) SetHeaders(headers map[string]string) Cora {
-	for k, v := range headers {
-		c.req.Header.Add(k, v)
-	}
-	return c
-}
-
-// AddHeaders add additional headers for a single request, apply it to all requests using Request method
-func (c *cora) AddHeaders(headers map[string]string) Cora {
-	for k, v := range headers {
-		c.req.Header.Set(k, v)
-	}
+func (c *cora) SetHeaders(headers ...Header) Cora {
+	c.headers = headers
 	return c
 }
 
 // Get request
-func (c *cora) Get(u string) Response {
-	return c.make(http.MethodGet, u, nil)
+func (c *cora) Get(u string, h ...Header) Response {
+	return c.make(http.MethodGet, u, nil, h...)
 }
 
 // Head request
-func (c *cora) Head(u string) Response {
-	return c.make(http.MethodHead, u, nil)
+func (c *cora) Head(u string, h ...Header) Response {
+	return c.make(http.MethodHead, u, nil, h...)
 }
 
 // Post request
-func (c *cora) Post(u string, b []byte) Response {
-	return c.make(http.MethodPost, u, b)
+func (c *cora) Post(u string, b []byte, h ...Header) Response {
+	return c.make(http.MethodPost, u, b, h...)
 }
 
 // Put request
-func (c *cora) Put(u string, b []byte) Response {
-	return c.make(http.MethodPut, u, b)
+func (c *cora) Put(u string, b []byte, h ...Header) Response {
+	return c.make(http.MethodPut, u, b, h...)
 }
 
 // Delete request
-func (c *cora) Delete(u string) Response {
-	return c.make(http.MethodDelete, u, nil)
+func (c *cora) Delete(u string, h ...Header) Response {
+	return c.make(http.MethodDelete, u, nil, h...)
 }
 
 // Options request
-func (c *cora) Options(u string) Response {
-	return c.make(http.MethodOptions, u, nil)
+func (c *cora) Options(u string, h ...Header) Response {
+	return c.make(http.MethodOptions, u, nil, h...)
 }
 
 // Trace request
-func (c *cora) Trace(u string) {}
+func (c *cora) Trace(u string, h ...Header) {}
 
 // Patch request
-func (c *cora) Patch(u string, b []byte) Response {
-	return c.make(http.MethodPatch, u, b)
+func (c *cora) Patch(u string, b []byte, h ...Header) Response {
+	return c.make(http.MethodPatch, u, b, h...)
+}
+
+// headers used by a single request
+func (c *cora) setHeaders(headers []Header, req *http.Request) {
+	for _, v := range headers {
+		for _, h := range v.Values {
+			req.Header.Add(v.Name, h)
+		}
+	}
 }
 
 // make manage the http request
-func (c *cora) make(method string, url string, body []byte) Response {
+func (c *cora) make(method string, url string, b []byte, headers ...Header) Response {
+	// check body content
+	var body io.Reader
+	if len(b) > 0 {
+		body = bytes.NewBuffer(b)
+		contentType := http.DetectContentType(b)
+		log.Println(contentType)
+	}
 
-	req, err := http.NewRequest(method, url, nil)
+	// create new request
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return Response{Err: err}
 	}
 
-	// Set custom header
-	if c.req.Header != nil {
-		req.Header = c.req.Header
+	// set custom host
+	if len(c.host) > 0 {
+		req.Host = c.host
 	}
 
-	// Set custom host
-	if len(c.req.Host) > 0 {
-		req.Host = c.req.Host
+	// set custom header
+	if len(c.headers) > 0 {
+		c.setHeaders(c.headers, req)
 	}
 
-	// Do request
+	// set given headers for the request
+	if len(headers) > 0 {
+		c.setHeaders(headers, req)
+	}
+
+	log.Println("Type", req.Header.Values("Content-Type"))
+
+	// do request
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return Response{Err: err}
 	}
 	defer resp.Body.Close()
 
-	// Check status code
-	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
-	if !statusOK {
-		return Response{Err: err, Code: resp.StatusCode}
-	}
-
-	// Read body
+	// read body
 	if resp.Body != nil {
 		body, err := ioutil.ReadAll(resp.Body)
-		return Response{Body: body, Code: resp.StatusCode, Raw: resp, Err: err}
+		res := Response{Body: body, Code: resp.StatusCode, Raw: resp, Err: err}
+		// check status code
+		statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
+		if !statusOK {
+			res.Err = errors.New(http.StatusText(resp.StatusCode))
+		}
+		// response
+		return res
 	}
 
+	// response
 	return Response{Raw: resp, Code: resp.StatusCode}
 }
